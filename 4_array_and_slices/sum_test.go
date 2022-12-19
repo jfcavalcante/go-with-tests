72efc06a827c66982154c8b63b288c296a4a0399
Jhonathan Cavalcante <jhonathan.cavalcante@luizalabs.com>
2022-12-18 21:52:22 -0300

    chore: test

--- 4_array_and_slices/sum_test.go
+++ 4_array_and_slices/sum_test.go
@@ -8,10 +8,10 @@ import (
 func TestSum(t *testing.T) {
 
 	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+		numbers := []int{1, 2, 3, 4}
 
 		got := Sum(numbers)
-		want := 6
+		want := 10
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
