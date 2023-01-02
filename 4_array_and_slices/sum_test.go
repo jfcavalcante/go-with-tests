cfe1cf2b46a98dee46e0f2bd014f0c49edad3057
Jhonathan Cavalcante <jhonathan.cavalcante@luizalabs.com>
2023-01-01 21:28:05 -0300

    chore: add testing changes

--- 4_array_and_slices/sum_test.go
+++ 4_array_and_slices/sum_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// go test -cover to see coverage
 func TestSum(t *testing.T) {
 
 	t.Run("collection of any size", func(t *testing.T) {
@@ -24,6 +25,8 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
+	// Arrays are deepely equal when their corresponding elements are deep equal
+	// Two values of identical Type
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
